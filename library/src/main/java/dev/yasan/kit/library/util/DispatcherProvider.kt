69344d23d21cbaf54c8708adbf9e1b5a5da3743d
YASAN <yasandev@gmail.com>
2022-02-04 00:12:24 +0330

    Create DispatcherProvider interface

---
+++ library/src/main/java/dev/yasan/kit/library/util/DispatcherProvider.kt
@@ -0,0 +1,16 @@
+package dev.yasan.kit.library.util
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+/**
+ * An interface meant for providing Coroutines' [Dispatchers] as Singletons using Hilt.
+ *
+ * @see Dispatchers
+ */
+interface DispatcherProvider {
+    val main: CoroutineDispatcher
+    val io: CoroutineDispatcher
+    val default: CoroutineDispatcher
+    val unconfined: CoroutineDispatcher
+}
