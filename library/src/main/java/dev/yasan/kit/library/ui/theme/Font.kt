2656682603dc31070fb694a41edbd2bc8ea4703c
YASAN <yasandev@gmail.com>
2022-02-03 22:45:06 +0330

    Add Rubik font family

---
+++ library/src/main/java/dev/yasan/kit/library/ui/theme/Font.kt
@@ -0,0 +1,28 @@
+package dev.yasan.kit.library.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import dev.yasan.kit.library.R
+
+val rubikFamily = FontFamily(
+    Font(R.font.rubik_black, FontWeight.Black, FontStyle.Normal),
+    Font(R.font.rubik_blackitalic, FontWeight.Black, FontStyle.Italic),
+
+    Font(R.font.rubik_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.rubik_bolditalic, FontWeight.Bold, FontStyle.Italic),
+
+    Font(R.font.rubik_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.rubik_lightitalic, FontWeight.Light, FontStyle.Italic),
+
+    Font(R.font.rubik_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.rubik_mediumitalic, FontWeight.Medium, FontStyle.Italic),
+
+    Font(R.font.rubik_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.rubik_italic, FontWeight.Normal, FontStyle.Italic),
+)
+
+val rubikMonoFamily = FontFamily(
+    Font(R.font.rubik_mono)
+)
