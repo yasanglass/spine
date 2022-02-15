d66c77f8d5030a2161c21fdbde5e3b088b7d5faa
YASAN <yasandev@gmail.com>
2022-02-15 17:02:39 +0330

    Improve YasanBrandingFooter

--- compose/src/main/java/dev/yasan/kit/compose/parts/branding/YasanFooterBranding.kt
+++ compose/src/main/java/dev/yasan/kit/compose/parts/branding/YasanFooterBranding.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import dev.yasan.kit.compose.R
 import dev.yasan.kit.compose.foundation.grid
@@ -18,17 +19,19 @@ import dev.yasan.kit.compose.foundation.grid
 @Composable
 fun YasanBrandingFooter(
     modifier: Modifier = Modifier,
-    topSpacerSize: Dp = grid(8),
-    bottomSpacerSize: Dp = grid(8),
+    spacerTop: Dp = grid(8),
+    spacerBottom: Dp = grid(8),
     logoHeight: Dp = grid(6),
-    crossFade: Boolean = true
+    crossFade: Boolean = true,
 ) {
 
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Spacer(Modifier.requiredHeight(topSpacerSize))
+        if (spacerTop > 0.dp) {
+            Spacer(Modifier.requiredHeight(spacerTop))
+        }
         Image(
             rememberImagePainter(
                 data = R.drawable.ic_yasan_logo_text2_color,
@@ -39,8 +42,9 @@ fun YasanBrandingFooter(
             contentDescription = null,
             modifier = Modifier.requiredHeight(logoHeight)
         )
-        Spacer(Modifier.requiredHeight(bottomSpacerSize))
+        if (spacerBottom > 0.dp) {
+            Spacer(Modifier.requiredHeight(spacerBottom))
+        }
     }
-
 
 }
