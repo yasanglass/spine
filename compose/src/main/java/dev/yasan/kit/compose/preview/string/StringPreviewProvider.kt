e56e0c28c3f92738538a50651722391163397e69
YASAN <yasandev@gmail.com>
2022-02-20 01:35:44 +0330

    Create StringPreviewProvider

---
+++ compose/src/main/java/dev/yasan/kit/compose/preview/string/StringPreviewProvider.kt
@@ -0,0 +1,23 @@
+package dev.yasan.kit.compose.preview.string
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+
+/**
+ * Provides preview parameters for nullable [Boolean].
+ */
+class StringPreviewProvider : PreviewParameterProvider<String> {
+
+    override val values = sequenceOf(
+        "Cum assimilatio experimentum, omnes sagaes imperium secundus, nobilis galluses.",
+        "Nutrix de lotus solitudo, prensionem barcas!",
+        "Ecce, messor!",
+        "Rusticus, albus extums patienter examinare de ferox, grandis vigil.",
+        "Peregrinatione tandem ducunt ad azureus animalis.",
+        "Lamia, fermium, et abaculus.",
+        "Ecce, noster luna!",
+        "Neuter imber diligenter attrahendams homo est.",
+        "Nunquam contactus devirginato.",
+        "Turpiss congregabo!"
+    )
+
+}
