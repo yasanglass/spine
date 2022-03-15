91bf8f0955a72e08b22c52aab4606d953cb56b0f
YASAN <yasandev@gmail.com>
2022-03-15 17:04:13 +0330

    Create KeyboardHelper

---
+++ core/src/main/java/dev/yasan/kit/core/helper/KeyboardHelper.kt
@@ -0,0 +1,28 @@
+package dev.yasan.kit.core.helper
+
+import android.app.Activity
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.view.inputmethod.InputMethodManager
+
+object KeyboardHelper {
+
+    /**
+     * Tries to close the keyboard if its open.
+     *
+     * @return If keyboard was closed.
+     */
+    fun closeKeyboard(activity: Activity): Boolean {
+        return try {
+            val inputManager = activity.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            inputManager.hideSoftInputFromWindow(
+                activity.currentFocus!!.windowToken,
+                InputMethodManager.HIDE_NOT_ALWAYS
+            )
+            true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
+    }
+
+}
