71a19a1cdf9c5c6a0cd34139cb7c9cab4530c788
Yasan <yasandev@gmail.com>
2022-06-23 00:29:19 +0430

    Create ExpandAndShrink.kt

---
+++ compose/src/main/java/dev/yasan/kit/compose/animation/ExpandAndShrink.kt
@@ -0,0 +1,17 @@
+package dev.yasan.kit.compose.animation
+
+import androidx.compose.animation.*
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ExpandAndShrink(
+    visible: Boolean,
+    content: @Composable AnimatedVisibilityScope.() -> Unit,
+) {
+    AnimatedVisibility(
+        visible = visible,
+        enter = expandVertically() + fadeIn(),
+        exit = shrinkVertically() + fadeOut(),
+        content = content,
+    )
+}
