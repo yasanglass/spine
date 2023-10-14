9ed9f7524e225be055848d0f7030ec7c1c7cdb4d
Yasan Glass <contact@yasan.glass>
2023-10-14 23:53:30 +0200

    Recreate YasanBrandingFooter

---
+++ spine-compose/src/main/kotlin/glass/yasan/spine/compose/branding/YasanBrandingFooter.kt
@@ -0,0 +1,59 @@
+package glass.yasan.spine.compose.branding
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import glass.yasan.spine.compose.R
+import glass.yasan.spine.compose.foundation.grid
+
+@Composable
+fun YasanBrandingFooter(
+    modifier: Modifier = Modifier,
+    spacerTop: Dp = grid(8),
+    spacerBottom: Dp = grid(8),
+    logoHeight: Dp = grid(6),
+    crossFade: Boolean = true,
+    colors: LogoColors = LogoColors.DarkLight,
+) {
+
+    Column(
+        modifier = modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if (spacerTop > 0.dp) {
+            Spacer(Modifier.requiredHeight(spacerTop))
+        }
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(colors.resolveImageData())
+                .crossfade(crossFade)
+                .build(),
+            contentDescription = null,
+            modifier = Modifier.requiredHeight(logoHeight)
+        )
+        if (spacerBottom > 0.dp) {
+            Spacer(Modifier.requiredHeight(spacerBottom))
+        }
+    }
+}
+
+enum class LogoColors {
+    DarkLight,
+    DarkLightInverted,
+}
+
+private fun LogoColors.resolveImageData(): Int =
+    when (this) {
+        LogoColors.DarkLight -> R.drawable.ic_yasan_logo_text2_color
+        LogoColors.DarkLightInverted -> R.drawable.ic_yasan_logo_text2_color_inverted
+    }
