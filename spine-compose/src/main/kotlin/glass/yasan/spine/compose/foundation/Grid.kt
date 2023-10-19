6cbe574a51f0934e0236417adb41707bce0b447d
Yasan Glass <contact@yasan.glass>
2023-10-20 01:31:17 +0200

    Set deprecation level of grid() to error

--- spine-compose/src/main/kotlin/glass/yasan/spine/compose/foundation/Grid.kt
+++ spine-compose/src/main/kotlin/glass/yasan/spine/compose/foundation/Grid.kt
@@ -15,21 +15,24 @@ inline val Float.grid: Dp get() = gridUnit * this
 
 @Deprecated(
     "Migrate to the new Float.grid extension function",
-    replaceWith = ReplaceWith("multiplier.grid")
+    replaceWith = ReplaceWith("multiplier.grid"),
+    level = DeprecationLevel.ERROR,
 )
 @Stable
 fun grid(multiplier: Float) = gridUnit * multiplier
 
 @Deprecated(
     "Migrate to the new Int.grid extension function",
-    replaceWith = ReplaceWith("multiplier.grid")
+    replaceWith = ReplaceWith("multiplier.grid"),
+    level = DeprecationLevel.ERROR,
 )
 @Stable
 fun grid(@IntRange(from = 2) multiplier: Int) = gridUnit * multiplier
 
 @Deprecated(
     "Migrate to the new Int.grid extension function",
-    replaceWith = ReplaceWith("1.grid")
+    replaceWith = ReplaceWith("1.grid"),
+    level = DeprecationLevel.ERROR,
 )
 @Stable
 fun grid() = gridUnit
