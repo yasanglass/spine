a58c5bed8ebf0b30b535242c892f1144ff3acee1
Yasan Ghaffarian <yasan.ghaffarian@kiwi.com>
2024-02-28 23:30:47 +0100

    Create FlowExtensions.kt

---
+++ spine-compose/src/main/kotlin/glass/yasan/spine/compose/extension/FlowExtensions.kt
@@ -0,0 +1,12 @@
+package glass.yasan.spine.compose.extension
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+
+fun <T> Flow<T>.firstBlocking() = runBlocking { first() }
+
+@Composable
+fun <T> Flow<T>.collectAsStateBlocking() = collectAsState(initial = firstBlocking())
