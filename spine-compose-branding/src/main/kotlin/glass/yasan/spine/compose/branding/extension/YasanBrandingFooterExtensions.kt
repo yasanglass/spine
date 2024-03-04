2ad6dfb991eba9956662bfb83a8b0c27d5d5fec7
Yasan Ghaffarian <yasan.ghaffarian@kiwi.com>
2024-03-04 22:38:04 +0100

    Create YasanBrandingFooterExtensions

---
+++ spine-compose-branding/src/main/kotlin/glass/yasan/spine/compose/branding/extension/YasanBrandingFooterExtensions.kt
@@ -0,0 +1,29 @@
+package glass.yasan.spine.compose.branding.extension
+
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import glass.yasan.spine.compose.branding.YasanBrandingFooter
+import glass.yasan.spine.compose.branding.YasanColors
+import glass.yasan.spine.compose.foundation.grid
+
+fun LazyListScope.yasanBrandingFooter(
+    modifier: Modifier = Modifier,
+    spacerTop: Dp = 8.grid,
+    spacerBottom: Dp = 8.grid,
+    logoHeight: Dp = 6.grid,
+    crossFade: Boolean = true,
+    colors: YasanColors = YasanColors.DarkLight,
+) {
+    item {
+        YasanBrandingFooter(
+            modifier = modifier,
+            spacerTop = spacerTop,
+            spacerBottom = spacerBottom,
+            logoHeight = logoHeight,
+            crossFade = crossFade,
+            colors = colors,
+        )
+    }
+}
+ 
