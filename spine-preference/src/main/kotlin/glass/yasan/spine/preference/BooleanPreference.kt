bd801943ec2623c26d49fea928cf8ac1944800dc
Yasan Ghaffarian <yasan.ghaffarian@kiwi.com>
2024-12-25 21:48:14 +0100

    Remove spine-preference

--- spine-preference/src/main/kotlin/glass/yasan/spine/preference/BooleanPreference.kt
+++
@@ -1,7 +0,0 @@
-package glass.yasan.spine.preference
-
-import com.patrykandpatrick.opto.domain.Preference
-
-suspend fun Preference<Boolean>.toggle() {
-    transform { currentValue -> !currentValue }
-}
